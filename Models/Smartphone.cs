189138681ed91bd432b5be4056e077d6fd627536
Eduardo Silva <eduardo.fernandes_@outlook.com>
2024-09-21 17:23:12 -0300

    Desafio POO concluido

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,26 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string Imei { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            Imei = imei;
+            Memoria = memoria;
         }
 
         public void Ligar()
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"{Modelo} Ligando...");
         }
 
         public void ReceberLigacao()
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($" {Modelo} Recebendo ligação...");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
-﻿using DesafioPOO.Models;
+﻿using System.ComponentModel;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+
+        Smartphone c1 = new Iphone("11999999999", "iPhone 15", "123456789", 500);
+        c1.Ligar();
+        c1.ReceberLigacao();
+        c1.InstalarAplicativo("Tiktok");
+
+        
+        Smartphone c2 = new Nokia("11900000000", "Nokia 3", "987654321", 10);
+        c2.Ligar();
+        c2.ReceberLigacao();
+        c2.InstalarAplicativo("");
